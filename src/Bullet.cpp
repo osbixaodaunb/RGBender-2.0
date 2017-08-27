b4354dda098c0a709325e74c0160437c3ade5450
Guilherme Sant'Ana <guilhermefuturo@hotmail.com>
2017-08-27 19:10:28 -0300

    folha estilo 2

--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -1,3 +1,4 @@
+/*Copyright 2017 MIT*/
 #include "Bullet.h"
 #include "SDLGameObject.h"
 #include "Vector2D.h"
@@ -12,14 +13,14 @@
 #include <iostream>
 #include <string>
 #include "Childmaiden.h"
-using namespace engine;
+
+// using namespace engine;
 
 Bullet::~Bullet() {
   INFO("Bullet destroyed")
 }
 
 Bullet::Bullet(Enemy *p_boss) : SDLGameObject() {
-
   setBoss(p_boss);
 
   timeToLive = 1000;
@@ -27,95 +28,105 @@ Bullet::Bullet(Enemy *p_boss) : SDLGameObject() {
   m_venemous = false;
 }
 
-void Bullet::load(const LoaderParams* pParams) {
-  m_velocity = Vector2D(0,0);
+void Bullet::load(const engine::LoaderParams* pParams) {
+  m_velocity = engine::Vector2D(0, 0);
 
   SDLGameObject::load(pParams);
 }
 
-double Bullet::rotateTowards(Vector2D pPosition) {
-  Vector2D target = InputHandler::Instance().getMousePosition() - pPosition;
+double Bullet::rotateTowards(engine::Vector2D pPosition) {
+  engine::Vector2D target = engine::InputHandler::Instance().
+      getMousePosition() - pPosition;
   target = target.norm();
 
-  Vector2D horizontal(0,1);
+  engine::Vector2D horizontal(0, 1);
 
-  return Vector2D::angle(target, Vector2D(0, 1));
+  return engine::Vector2D::angle(target, engine::Vector2D(0, 1));
 }
-void Bullet::load(Vector2D pVelocity, Vector2D pPosition) {
+void Bullet::load(engine::Vector2D pVelocity, engine::Vector2D pPosition) {
   double angle = rotateTowards(pPosition);
 
   m_moveSpeed = 5;
-  LoaderParams* pParams = new LoaderParams(pPosition.getX(), pPosition.getY(), 16, 16, "bullet", 0, 0, 0, angle);
+  engine::LoaderParams* pParams = new engine::LoaderParams(pPosition.getX(),
+          pPosition.getY(),
+       16, 16, "bullet", 0, 0, 0, angle);
   SDLGameObject::load(pParams);
 
   m_currentFrame = 0;
-  bornTime = Timer::Instance().step();
-	m_velocity = pVelocity.norm() * m_moveSpeed;
+  bornTime = engine::Timer::Instance().step();
+  m_velocity = pVelocity.norm() * m_moveSpeed;
 }
 
-void Bullet::draw(){
-	SDLGameObject::draw();
+void Bullet::draw() {
+  SDLGameObject::draw();
 }
 
-void Bullet::update(){
-	//std::cout << "Bullet top: " << getPosition().getY() + (getHeight() - getCollider().getHeight())/2 << std::endl;
-	m_position += m_velocity;
-	m_currentFrame = int(((SDL_GetTicks() / (300)) % 4));
-	if(Timer::Instance().step() >= bornTime + timeToLive){
-		m_active = false;
-		m_venemous = false;
-		Game::Instance().getStateMachine()->currentState()->removeGameObject(this);
-	}
-	checkCollision();
-
+void Bullet::update() {
+  // std::cout << "Bullet top: " << getPosition().getY() +
+  // (getHeight() - getCollider().getHeight())/2 << std::endl;
+  m_position += m_velocity;
+  m_currentFrame = static_cast<int>(((SDL_GetTicks() / (300)) % 4));
+  if (engine::Timer::Instance().step() >= bornTime + timeToLive) {
+    m_active = false;
+    m_venemous = false;
+    engine::Game::Instance().getStateMachine()->currentState()->
+        removeGameObject(this);
+  }
+  checkCollision();
 }
 
-void Bullet::checkCollision(){
-	if(m_active){
-		Vector2D pos = m_boss->getPosition();
-		Vector2D thisPos = getPosition();
-		
-		
-		for(auto obj : engine::Game::Instance().getStateMachine()->currentState()->getShieldObjects()){
-			if(Physics::Instance().checkCollision(dynamic_cast<SDLGameObject*>(obj), dynamic_cast<SDLGameObject*>(this))){
-				if(dynamic_cast<Childmaiden*>(obj)->getVisibility()){
-					INFO("REMOVENDO BALA");
-					m_active = false;
-					Game::Instance().getStateMachine()->currentState()->removeGameObject(this);
-					INFO("Bullet collided with shield");
-				}
-				else{
-					break;
-				}
-			}
-		}
+void Bullet::checkCollision() {
+  if (m_active) {
+    engine::Vector2D pos = m_boss->getPosition();
+    engine::Vector2D thisPos = getPosition();
 
-		if(Physics::Instance().checkCollision(dynamic_cast<SDLGameObject*>(m_boss), dynamic_cast<SDLGameObject*>(this))){
-			m_active = false;
-			Game::Instance().getStateMachine()->currentState()->removeGameObject(this);
-			m_boss->takeDamage(10);
-			if(m_venemous){
-				INFO("VENEMOUS TRUE");
-				m_collided = true;
-				uint8_t* pixels = new uint8_t[3];
-				pixels[0] = 152;
-				pixels[1] = 255;
-				pixels[2] = 156;
-				TheTextureManager::Instance().changeColorPixels(pixels, "RAG");
-				m_boss->setEnemyTime(1000);
-			}
-			
-			int score = Game::Instance().getScore();
-			Game::Instance().setScore(score + 10);
-			TextureManager::Instance().loadText(std::to_string(Game::Instance().getScore()), "assets/fonts/Lato-Regular.ttf", "score", {255,255,255}, 50, Game::Instance().getRenderer());
-			INFO("Bullet collided with boss");
-		}
+    for (auto obj : engine::Game::Instance().getStateMachine()->
+                  currentState()->getShieldObjects()) {
+      if (engine::Physics::Instance().
+              checkCollision(dynamic_cast<SDLGameObject*>(obj),
+                  dynamic_cast<SDLGameObject*>(this))) {
+        if (dynamic_cast<Childmaiden*>(obj)->getVisibility()) {
+          INFO("REMOVENDO BALA");
+          m_active = false;
+          engine::Game::Instance().getStateMachine()->
+              currentState()->removeGameObject(this);
+          INFO("Bullet collided with shield");
+        } else {
+          break;
+        }
+      }
+    }
 
+    if (engine::Physics::Instance().
+            checkCollision(dynamic_cast<SDLGameObject*>(m_boss),
+                dynamic_cast<SDLGameObject*>(this))) {
+      m_active = false;
+      engine::Game::Instance().getStateMachine()->currentState()->
+          removeGameObject(this);
+      m_boss->takeDamage(10);
+      if (m_venemous) {
+        INFO("VENEMOUS TRUE");
+        m_collided = true;
+        uint8_t* pixels = new uint8_t[3];
+        pixels[0] = 152;
+        pixels[1] = 255;
+        pixels[2] = 156;
+        engine::TheTextureManager::Instance().changeColorPixels(pixels, "RAG");
+        m_boss->setEnemyTime(1000);
+      }
 
-	}
+      int score = engine::Game::Instance().getScore();
+      engine::Game::Instance().setScore(score + 10);
+      engine::TextureManager::Instance().
+          loadText(std::to_string(engine::Game::Instance().getScore()),
+          "assets/fonts/Lato-Regular.ttf", "score",
+          {255, 255, 255}, 50, engine::Game::Instance().getRenderer());
+      INFO("Bullet collided with boss");
+    }
+  }
 }
 
-void Bullet::clean(){
-	SDLGameObject::clean();
+void Bullet::clean() {
+  SDLGameObject::clean();
 }
 
