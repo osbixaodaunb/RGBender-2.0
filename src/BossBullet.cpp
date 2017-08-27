b82c0f63e49416d729a03acf886388a303bfc679
Guilherme Sant'Ana <guilhermefuturo@hotmail.com>
2017-08-27 16:19:56 -0300

    Folha de estilo 1

--- src/BossBullet.cpp
+++ src/BossBullet.cpp
@@ -1,3 +1,4 @@
+/*Copyright 2017 MIT*/
 #include "BossBullet.h"
 #include "SDLGameObject.h"
 #include "Vector2D.h"
@@ -15,105 +16,113 @@
 #include <ctime>
 
 
-using namespace engine;
+// using namespace engine;
 
-BossBullet::~BossBullet(){
-	INFO("Boss Bullet destroyed")
+BossBullet::~BossBullet() {
+  INFO("Boss Bullet destroyed")
 }
 
-BossBullet::BossBullet(Player *target) : SDLGameObject(){
+BossBullet::BossBullet(Player *target) : SDLGameObject() {
+  std::srand(std::time(0));
 
-	std::srand(std::time(0));
+  setPlayer(target);
+  timeToLive = 5000;
+  m_active = true;
 
-	setPlayer(target);
-	timeToLive = 5000;
-	m_active = true;
-
-	m_textureID = "bullet" + std::to_string(1 + std::rand()%9);
+  m_textureID = "bullet" + std::to_string(1 + std::rand()%9);
 }
 
-void BossBullet::load(const LoaderParams* pParams){
-	std::srand(std::time(0));
-	m_velocity = Vector2D(0,0);
-	
-	m_textureID = "bullet" + std::to_string(1 + std::rand()%9);
-	SDLGameObject::load(pParams);
+void BossBullet::load(const engine::LoaderParams* pParams) {
+  std::srand(std::time(0));
+  m_velocity = engine::Vector2D(0, 0);
+
+  m_textureID = "bullet" + std::to_string(1 + std::rand()%9);
+  SDLGameObject::load(pParams);
 }
 
-double BossBullet::rotateTowards(Vector2D pPosition){
-	Vector2D target = InputHandler::Instance().getMousePosition() - pPosition;
-	target = target.norm();
+double BossBullet::rotateTowards(engine::Vector2D pPosition) {
+  engine::Vector2D target =
+      engine::InputHandler::Instance().getMousePosition() - pPosition;
+  target = target.norm();
 
-	Vector2D horizontal(0,1);
+  engine::Vector2D horizontal(0, 1);
 
-	return Vector2D::angle(target, Vector2D(0, 1));
+  return engine::Vector2D::angle(target, engine::Vector2D(0, 1));
 }
-void BossBullet::load(Vector2D pVelocity, Vector2D pPosition){
-	std::srand(std::time(0));
-	double m_angle = 0;
+void BossBullet::load(engine::Vector2D pVelocity, engine::Vector2D pPosition) {
+  std::srand(std::time(0));
+  double m_angle = 0;
 
-	m_moveSpeed = 3;
-	m_textureID = "bullet" + std::to_string(1 + std::rand()%9);
-	LoaderParams* pParams = new LoaderParams(pPosition.getX(), pPosition.getY(), 48, 48, m_textureID, 0, 0, 0, m_angle);
-	SDLGameObject::load(pParams);
+  m_moveSpeed = 3;
+  m_textureID = "bullet" + std::to_string(1 + std::rand()%9);
+  engine::LoaderParams* pParams = new engine::LoaderParams(pPosition.getX(),
+          pPosition.getY(), 48, 48, m_textureID,
+          0, 0, 0, m_angle);
+  SDLGameObject::load(pParams);
 
-	m_currentFrame = 0;
-	bornTime = Timer::Instance().step();
-	m_velocity = pVelocity.norm() * m_moveSpeed;
-	//m_velocity = Vector2D(0.5, 0.5);
-	
-	Vector2D norm = pVelocity.norm();
-	normal = Vector2D(norm.getY(), -norm.getX());
-	originTime = Timer::Instance().step();
-}
+  m_currentFrame = 0;
+  bornTime = engine::Timer::Instance().step();
+  m_velocity = pVelocity.norm() * m_moveSpeed;
+  // m_velocity = Vector2D(0.5, 0.5);
 
-void BossBullet::draw(){
-	SDLGameObject::draw();
-}
-
-void BossBullet::update(){
-	Vector2D tmp = m_velocity;
-	
-	double sign = sin((Timer::Instance().step() - originTime)/125);
-	sign /= fabs(sign);
-
-	sign *= 1;
-	m_velocity += Vector2D(normal.getX()*sign, normal.getY()*sign);
-
-	m_position += m_velocity;
-	m_velocity = tmp;
-
-	
-
-	if(Timer::Instance().step() >= bornTime + timeToLive){
-		m_active = false;
-		Game::Instance().getStateMachine()->currentState()->removeGameObject(this);
-	}
-
-	checkCollision();
-
+  engine::Vector2D norm = pVelocity.norm();
+  normal = engine::Vector2D(norm.getY(), -norm.getX());
+  originTime = engine::Timer::Instance().step();
 }
 
-void BossBullet::checkCollision(){
-	if(m_active){
-		Vector2D pos = m_player->getPosition();
-		Vector2D thisPos = getPosition();
-		
-		if(Physics::Instance().checkCollision(dynamic_cast<SDLGameObject*>(m_player), dynamic_cast<SDLGameObject*>(this))){
-			m_active = false;
-			Game::Instance().getStateMachine()->currentState()->removeGameObject(this);
-			//INFO("Bullet collided");
-			//INFO("PLAYER LOST THE GAME");
-			if(!m_player->getShieldActive())
-				m_player->setLife((m_player->getLife()) - 1);
-			else if(m_player->getShieldActive())
-				m_player->setShieldHits();
-			//Game::Instance().getStateMachine()->changeState(new GameOverState());
-		}
-	}
+void BossBullet::draw() {
+  SDLGameObject::draw();
 }
 
-void BossBullet::clean(){
-	SDLGameObject::clean();
+void BossBullet::update() {
+  engine::Vector2D tmp = m_velocity;
+
+  double sign = sin((engine::Timer::Instance().step() - originTime)/125);
+  sign /= fabs(sign);
+
+  sign *= 1;
+  m_velocity += engine::Vector2D(normal.getX()*sign, normal.getY()*sign);
+
+  m_position += m_velocity;
+  m_velocity = tmp;
+
+
+
+  if (engine::Timer::Instance().step() >= bornTime + timeToLive) {
+    m_active = false;
+    engine::Game::Instance().getStateMachine()->
+        currentState()->removeGameObject(this);
+  }
+
+  checkCollision();
+}
+
+void BossBullet::checkCollision() {
+  if (m_active) {
+    engine::Vector2D pos = m_player->getPosition();
+    engine::Vector2D thisPos = getPosition();
+
+    if (engine::Physics::Instance().checkCollision(
+          dynamic_cast<SDLGameObject*>(m_player),
+          dynamic_cast<SDLGameObject*>(this))) {
+      m_active = false;
+      engine::Game::Instance().getStateMachine()->currentState()->
+          removeGameObject(this);
+      // INFO("Bullet collided");
+      // INFO("PLAYER LOST THE GAME");
+    if (!m_player->getShieldActive()) {
+        m_player->setLife((m_player->getLife()) - 1);
+      } else {
+        if (m_player->getShieldActive()) {
+        m_player->setShieldHits();
+        }
+      }
+      // Game::Instance().getStateMachine()->changeState(new GameOverState());
+    }
+  }
+}
+
+void BossBullet::clean() {
+  SDLGameObject::clean();
 }
 
--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -14,42 +14,42 @@
 #include "Childmaiden.h"
 using namespace engine;
 
-Bullet::~Bullet(){
-	INFO("Bullet destroyed")
+Bullet::~Bullet() {
+  INFO("Bullet destroyed")
 }
 
-Bullet::Bullet(Enemy *p_boss) : SDLGameObject(){
+Bullet::Bullet(Enemy *p_boss) : SDLGameObject() {
 
-	setBoss(p_boss);
+  setBoss(p_boss);
 
-	timeToLive = 1000;
-	m_active = true;
-	m_venemous = false;
+  timeToLive = 1000;
+  m_active = true;
+  m_venemous = false;
 }
 
-void Bullet::load(const LoaderParams* pParams){
-	m_velocity = Vector2D(0,0);
+void Bullet::load(const LoaderParams* pParams) {
+  m_velocity = Vector2D(0,0);
 
-	SDLGameObject::load(pParams);
+  SDLGameObject::load(pParams);
 }
 
-double Bullet::rotateTowards(Vector2D pPosition){
-	Vector2D target = InputHandler::Instance().getMousePosition() - pPosition;
-	target = target.norm();
+double Bullet::rotateTowards(Vector2D pPosition) {
+  Vector2D target = InputHandler::Instance().getMousePosition() - pPosition;
+  target = target.norm();
 
-	Vector2D horizontal(0,1);
+  Vector2D horizontal(0,1);
 
-	return Vector2D::angle(target, Vector2D(0, 1));
+  return Vector2D::angle(target, Vector2D(0, 1));
 }
-void Bullet::load(Vector2D pVelocity, Vector2D pPosition){
-	double angle = rotateTowards(pPosition);
+void Bullet::load(Vector2D pVelocity, Vector2D pPosition) {
+  double angle = rotateTowards(pPosition);
 
-	m_moveSpeed = 5;
-	LoaderParams* pParams = new LoaderParams(pPosition.getX(), pPosition.getY(), 16, 16, "bullet", 0, 0, 0, angle);
-	SDLGameObject::load(pParams);
+  m_moveSpeed = 5;
+  LoaderParams* pParams = new LoaderParams(pPosition.getX(), pPosition.getY(), 16, 16, "bullet", 0, 0, 0, angle);
+  SDLGameObject::load(pParams);
 
-	m_currentFrame = 0;
-	bornTime = Timer::Instance().step();
+  m_currentFrame = 0;
+  bornTime = Timer::Instance().step();
 	m_velocity = pVelocity.norm() * m_moveSpeed;
 }
 
